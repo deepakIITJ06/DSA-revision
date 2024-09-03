4eb3a189117f4b19cc339a36d3e096465cf57aaa
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Air.local>
2024-09-03 17:11:48 +0530

    top view of BT

--- articulation_point.cpp
+++ articulation_point.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int mod = 1e9+7;
 
 // 🔴🔴🔴🔴🔴🔴🔴 LOGIC 🔴🔴🔴🔴🔴🔴🔴
-// simply time of reaching store kr rahein hein --> along with ki minimum time kya ho sakta hai apart from parent
+// simply time of reaching store kr rahein hein --> along with ki minimum time kya ho sakta hai apart from parent (means ki agar child parent hai then--> continue BHULNA NAHI HAI ye case) and 🔴 visited node too
 // apart from "PARENT node and VISITED node" agar koi node hai dfs me then uske minimum time se compare kr lein and visited node se compare krte time ham "bridge" ki tarah "minTime" se compare nahi kr sakte , we can only compare with time[child] --> jo ki visited hai
-// then after coming back to that node --> check ki is node tak aane tak ka time se < (large) to nahin iske child tak aane ka time if "YES" --> that means ki wo ek bridge hai
+// then after coming back to that node --> check ki is node tak aane tak ka time se <= (large) to nahin iske child tak aane ka time if "YES" --> that means ki wo ek ARTICULATION point hai
 
 
 class Solution {
@@ -30,7 +30,7 @@ public:
             if(!vis[child]) {
                 dfs(child,node,adj);
                 lowest[node] = min(lowest[node],lowest[child]);
-                // here keep an eye on equality
+                // here keep an eye on 🔴 equality 🔴
                 if(lowest[child]>=time[node] && parent!=-1) {
                     mark[node] = 1;
                 }
