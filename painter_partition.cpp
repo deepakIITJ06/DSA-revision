c2658fe5ca8a067cafe1f64bbddf8fcb5987ce6d
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Air.local>
2024-10-01 16:11:50 +0530

    painters partition binary search on ANS

---
+++ painter_partition.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+using namespace std;
+#define int long long
+int mod = 1e9+7;
+
+// 🔴🔴🔴🔴🔴🔴🔴 LOGIC 🔴🔴🔴🔴🔴🔴🔴
+// simple --> Binary search on Ans
+// Binary search on ans ---> aise hi somewhat likha jayega...
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    //    Write your code here.
+    int low = 1;
+    int high = accumulate(begin(boards),end(boards),0);
+    int mid,painter,area,ans;
+    while(low<=high) {
+        mid = low+(high-low)/2;
+        area = 0;
+        painter = 0;
+        for(auto &el: boards) {
+            area += el;
+            if(area==mid) {
+                painter++;
+                area = 0;
+            }else if(area>mid) {
+                if(el>mid) {
+                    painter = 1e9;
+                    break;
+                }
+                painter++;
+                area = 0;
+                area += el;
+            }
+        }
+        if(area) painter++;
+        if(painter<=k) {
+            ans = mid;
+            high = mid-1;
+        }else {
+            low = mid+1;
+        }
+    }
+    return ans;
+}
