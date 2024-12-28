a9dcd461a8865d02172fa8a00fa4005671d05bc9
DEEPAK KUMAR <deepakkumar@DEEPAKs-MacBook-Air.local>
2024-12-29 01:33:06 +0530

    total subsets

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void sum(int a,int b) {
+    cout<<a+b<<endl;
+}
+
+signed main() {
+    sum(5,6);
+    return 0;
+}
--- total_subsets.cpp
+++ total_subsets.cpp
@@ -34,11 +34,11 @@ public:
         vector<vector<int>> ans;
         for(int i=0;i<(1<<n);i++) {
         vector<int> temp;
-        for(int j=0;j<nums.size();j++) {
-            if((i>>j)&1) { // here you can also do --> i & (1<<j)
-            temp.push_back(nums[j]);
+            for(int j=0;j<nums.size();j++) {
+                if((i>>j)&1) { // here you can also do --> i & (1<<j)
+                temp.push_back(nums[j]);
+                }
             }
-        }
         ans.push_back(temp);
         }
         return ans;
